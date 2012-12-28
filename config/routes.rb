d7c2c030fc2138417b948e385bb992910a118007
John Brunton <john_brunton@hotmail.co.uk>
2012-12-28 14:48:56 +0000

    fixed routes for devise callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MicroBlog::Application.routes.draw do
-  scope ":locale" do
+  scope "(:locale)" do
     ActiveAdmin.routes(self)
 
     match '/users/auth/:provider/callback' => 'authorizations#create' 
@@ -10,7 +10,7 @@ MicroBlog::Application.routes.draw do
     devise_scope :user do
       get "register", :to => "devise/registrations#new", :as => :register_user
       get "sign_in", :to => "devise/sessions#new", :as => :new_user_session
-      delete "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+      delete "sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
     end
     
     resources :users do
