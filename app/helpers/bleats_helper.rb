3951afdd7fceb4937a20702866dd1e486647123f
jbrunton <john_brunton@hotmail.co.uk>
2013-01-03 20:26:22 +0000

    removed hashtags from bleats for BDD demo purposes

--- app/helpers/bleats_helper.rb
+++ app/helpers/bleats_helper.rb
@@ -1,9 +1,2 @@
 module BleatsHelper
-  def linkify_tag(tag)
-    link_to tag, controller: 'search', action: 'index', q: tag
-  end
-
-  def linkify_content(content)
-    content.gsub(/#[A-Za-z0-9]+/) { |tag| linkify_tag(tag) }.html_safe
-  end
 end
